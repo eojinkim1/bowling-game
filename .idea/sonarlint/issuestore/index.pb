6e1d6bd1bd2cf6a04995eeb2c4b787b4bb5a558f
eojinkim1 <rladjwls999@gmail.com>
2025-01-16 18:49:02 +0900

    볼링게임 만들기 연습

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,20 @@
 build.gradle.kts,d/b/dbcff70658daf80b53ce624f6adcaa529df5ed8d
 X
 (src/main/java/github/eojinkim1/Main.java,2/d/2dff8fa7ced42bec5861f20d09733fee8a296e14
+^
+.src/main/java/bowling_game/eojinkim1/Main.java,b/b/bb993b902381becb1499ec0427958519bc60d694
+T
+$src/main/java/bowling_game/Main.java,3/9/3974015ebfd459024e33f04470e5df8162567a68
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+T
+$src/main/java/bowling_game/Game.java,a/c/ac33a22bacb1c439715c07f7f8c2d2d7162d10b8
+[
++src/main/java/bowling_game/BowlingGame.java,2/8/28e75dc863872620d165f2ad12919ef170458950
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8/8/88a9255124c95bdc913197c120a8d560edc59c8e
+S
+#.idea/sonarlint/issuestore/index.pb,9/f/9fe84ebb15faf917b7def6236dba604453cc61e0
--- src/main/java/bowling_game/BowlingGame.java
+++ src/main/java/bowling_game/BowlingGame.java
@@ -1,7 +1,77 @@
 package bowling_game;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Game {
+public class BowlingGame {
+    private static final int MAX_ROLLS = 21;
+    private final int[] rolls = new int[MAX_ROLLS];
+    private int currentRolls = 0;
 
+    public static void main(String[] args) {
+        BowlingGame game = new BowlingGame();
+
+        // 올바른 10개의 프레임 데이터
+        game.roll(10); // 1번째 프레임: 스트라이크
+        game.roll(7);
+        game.roll(3);  // 2번째 프레임: 스페어
+        game.roll(9);
+        game.roll(0);  // 3번째 프레임: 일반 프레임
+        game.roll(10); // 4번째 프레임: 스트라이크
+        game.roll(10); // 5번째 프레임: 스트라이크
+        game.roll(10); // 6번째 프레임: 스트라이크
+        game.roll(2);
+        game.roll(3);  // 7번째 프레임: 일반 프레임
+        game.roll(6);
+        game.roll(4);  // 8번째 프레임: 스페어
+        game.roll(7);
+        game.roll(2);  // 9번째 프레임: 일반 프레임
+        game.roll(10); // 10번째 프레임: 스트라이크
+        game.roll(10); // 추가 롤
+        game.roll(10); // 추가 롤
+
+        System.out.println("Total Score: " + game.scoreBoard());
+    }
+
+
+    public void roll(int pins) {
+        rolls[currentRolls++] = pins;
+    }
+
+
+    public int scoreBoard() {
+        int score = 0;
+        int rollIndex = 0;
+
+        for (int frame = 0; frame < 10; frame++) {
+            if (isStrike(rollIndex)) {
+                score += 10 + strikeBonus(rollIndex);
+                rollIndex++;
+            } else if (isSpare(rollIndex)) {
+                score += 10 + spareBonus(rollIndex);
+                rollIndex += 2;
+            } else {
+                score += NormalRoll(rollIndex);
+                rollIndex += 2;
+            }
+        }
+        return score;
+    }
+
+    private boolean isStrike(int rollIndex) {
+        return rolls[rollIndex] == 10;
+    }
+
+    private int strikeBonus(int rollIndex) {
+        return rolls[rollIndex + 1] + rolls[rollIndex + 2];
+    }
+
+    private boolean isSpare(int rollIndex) {
+        return rolls[rollIndex] + rolls[rollIndex + 1] == 10;
+    }
+
+    private int spareBonus(int rollIndex) {
+        return rolls[rollIndex + 2];
+    }
+
+    private int NormalRoll(int rollIndex) {
+        return rolls[rollIndex] + rolls[rollIndex + 1];
+    }
 }
